9a4cc78734ab3dd46406f97750e9118ffa9e63af
Saket Verma <saketverma@plexusmd.com>
2020-06-21 01:41:48 +0530

    Updates

---
+++ precache-manifest.48def57c3ad12933f0e70d6db17b9894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c819e1b7deedd90d3207643cce768c",
+    "url": "/saketVerma/index.html"
+  },
+  {
+    "revision": "b32e9198bbf1ef71add9",
+    "url": "/saketVerma/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e28a43d85a021e3e3807",
+    "url": "/saketVerma/static/js/2.a061eb6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saketVerma/static/js/2.a061eb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32e9198bbf1ef71add9",
+    "url": "/saketVerma/static/js/main.fe681c91.chunk.js"
+  },
+  {
+    "revision": "b9e9fa6507d6e989d917",
+    "url": "/saketVerma/static/js/runtime-main.7747ab14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saketVerma/static/media/logo.5d5d9eef.svg"
+  }
+]);
