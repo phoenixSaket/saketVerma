9197160d49aca54a63ae19f3e7f2c21c864a3fdb
Saket Verma <saketverma@plexusmd.com>
2020-06-21 02:20:09 +0530

    first draft

--- saketverma/src/App.js
+++ saketverma/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        Hello
-      </div>
+    <div className="homepage">
+      <Navbar style={{ backgroundColor: '#0D47A1' }} fixed="top" variant="dark">
+        <Navbar.Brand href="#home">Saket Verma</Navbar.Brand>
+      </Navbar>
+      <Container maxWidth="xl">
+        <div className="App">
+          Hello There. This is supposed to be my website.
+          Work is in progress.
+          Please fucking be patient !!
+        </div>
+      </Container>
     </div>
   );
 }
