b32ebfbb41347a7c57ddeebb9848230440db2401
Saket Verma <saketverma@plexusmd.com>
2020-07-02 19:45:00 +0530

    Images

--- saketverma/src/Components/ExperienceAndWork.js
+++ saketverma/src/Components/ExperienceAndWork.js
@@ -1,27 +1,68 @@
 import React from 'react';
 // import { Row, Col } from 'react-bootstrap';
 import "./ExperienceAndWork.css";
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Button } from 'react-bootstrap';
 import Company from './Company';
+import Gallery from 'react-photo-gallery';
+
+function openNewTab() {
+  window.open("https://play.google.com/store/apps/details?id=org.kustom.wallpaper&hl=en_IN");
+}
 
 function ExperienceAndWork() {
+  const photos = [
+    {
+      src: require('../Images/Photos/Flower.jpg'),
+      width: 2,
+      height: 2.5
+    },
+    {
+      src: require('../Images/Photos/Globe.png'),
+      width: 3,
+      height: 2
+    }, {
+      src: require('../Images/Photos/Bulb.png'),
+      width: 2.5,
+      height: 2
+    },
+    {
+      src: require('../Images/Photos/Cube.png'),
+      width: 2,
+      height: 2.5
+    },
+    {
+      src: require('../Images/Photos/Goa.png'),
+      width: 2,
+      height: 2.5
+    },
+    {
+      src: require('../Images/Photos/Tree.png'),
+      width: 2,
+      height: 2.5
+    },
+    {
+      src: require('../Images/Photos/Sky.png'),
+      width: 2,
+      height: 2.5
+    },
+  ];
   return (
     <div>
       <div className="boxShadow padding20 paddingBottom20 semi-white-bg">
         <div className="center-text heading-sub">
           EXPERIENCE
       </div>
-        <Company 
+        <Company
           companyName="PlexusMD"
           imageLink={require("../Images/PlexusMD.png")}
           type="Internship"
           workPeriod="Jan, 2020 - June, 2020"
-          workingAs= {[
+          workingAs={[
             "React Frontend Developer",
             "Search Engine Optimizations"
           ]}
-          workLink= {[
-            "https://www.plexusmd.com",
+          workLink={[
+            "www.plexusmd.com",
             "https://www.linkedin.com/company/plexusmd/"
           ]}
           aboutCompany="PlexusMD is an online professional network exclusively for medical professionals and healthcare organisations.
@@ -32,16 +73,16 @@ function ExperienceAndWork() {
           find & connect with other medical professionals and apply to the best jobs,
           fellowships and courses from leading institutions."
         />
-        <Company 
+        <Company
           companyName="ScrollRight"
           imageLink={require("../Images/sr_mukul.png")}
           type="Full Time"
           workPeriod="Since June, 2020"
-          workingAs= {[
+          workingAs={[
             "Software Developer"
           ]}
-          workLink= {[
-            "https://www.scrollright.com"
+          workLink={[
+            "www.scrollright.com"
           ]}
           aboutCompany="ScrollRight is a online freelance service provider and product building company where you can ask to create
           your own app, website, software. Also, we provided freelance support to any firm, company who needs it. Think Big.
@@ -51,7 +92,7 @@ function ExperienceAndWork() {
       <div className="workContainer boxShadow semi-white-bg" style={{ marginTop: '40px' }}>
         <div className="padding20">
           <div className="center-text heading-sub">
-            WORK
+            OTHER
           </div>
         </div>
         <div className="miniTitle text-center">
@@ -87,13 +128,23 @@ function ExperienceAndWork() {
             Design is basically set using semaphores and have similar concepts ike onClick event to change semaphores.
             <br /><br />
             <div className="text-center w-100 font-bold">
-              <a className="workLink" href="https://play.google.com/store/apps/details?id=org.kustom.wallpaper&hl=en_IN"
-                rel='noopener noreferrer' target="_blank">
-                {/* <i className="fa fa-android"></i> */}
+              <Button variant="info"
+                onClick={openNewTab}
+              >
+                {/* <a className="workLink" href="https://play.google.com/store/apps/details?id=org.kustom.wallpaper&hl=en_IN"
+                  rel='noopener noreferrer' target="_blank">
+                  <i className="fa fa-android"></i> */}
                 KLWP on Google Play Store
-            </a>
+              {/* </a> */}
+              </Button>
             </div>
           </div>
+        </div>
+        <div className="miniTitle text-center">
+          Photography
+        </div>
+        <div style={{ marginTop: '20px' }}>
+          <Gallery photos={photos} />
         </div>
       </div>
     </div>
