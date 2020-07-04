7705379232f81cf60c48c52113a3910dd8f93690
Saket Verma <saketverma@plexusmd.com>
2020-07-05 02:24:25 +0530

    image v3

--- saketverma/src/App.js
+++ saketverma/src/App.js
@@ -16,13 +16,13 @@ function App() {
       <Helmet>
         <title>Saket Verma</title>
         <meta name="description" content="Saket's Homepage / Digital Resume" />
-        <meta name="image" content="https://drive.google.com/file/d/1u1lse-22ofypGxgF4ebdRDAqekEXH2o_/view?usp=sharing" />
+        <meta name="image" content={require("./Images/myAvatar.png")} />
         <meta name="og:title" content="Saket Verma" />
         <meta name="og:description" content="Saket's Homepage / Digital Resume" />
-        <meta name="og:image" content="https://drive.google.com/file/d/1u1lse-22ofypGxgF4ebdRDAqekEXH2o_/view?usp=sharing" />
+        <meta name="og:image" content={require("./Images/myAvatar.png")} />
         <meta name="twitter:title" content="Saket Verma" />
         <meta name="twitter:description" content="Saket's Homepage / Digital Resume" />
-        <meta name="twitter:image" content="https://drive.google.com/file/d/1u1lse-22ofypGxgF4ebdRDAqekEXH2o_/view?usp=sharing" />
+        <meta name="twitter:image" content={require("./Images/myAvatar.png")} />
       </Helmet>
       <Container className="containerMain" size="md">
         <div className="navbar-container">
